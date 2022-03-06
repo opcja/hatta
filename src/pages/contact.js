2980f7ad10b123c8088947aa098f16c6fd3060da
opcja <piotrmajda7@gmail.com>
2022-03-06 08:09:55 +0100

    Add Thanks page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -208,7 +208,7 @@ export default function Contact() {
             }),
           })
             .then(() => {
-              alert("Send")
+              navigate("/thanks/")
             })
             .catch(error => alert(error))
         }}
---
+++ src/pages/thanks.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Thanks = () => {
+  return <p>Dziękujemy za wiadomość</p>
+}
+
+export default Thanks
